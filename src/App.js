d29289313e5f5afc1a1367dc9c88fa08e786c3e9
Wes Bryie <wesleybryie@protonmail.com>
2022-05-28 20:43:42 +0000

    I got stuck here, if anyone wants to help, do.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 // Import needed files/packages.
-import './App.css';
+import './App.css'
+import HandleClick from './HandleClick'
 
-var num;
-
-function HandleClick() {
-  num = 0;
-  var maxNumber = this.App.maxnumbox;
-  num = Math.floor((Math.random() * maxNumber) + 1);
-}
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Your number is: {num}
+          Your number is: {null}
         </p>
         <input id="maxnumbox" type="number"></input>
         <button onClick={HandleClick}>Generate Number</button>
@@ -22,6 +15,3 @@ function App() {
     </div>
   );
 }
-
-
-export default App;
